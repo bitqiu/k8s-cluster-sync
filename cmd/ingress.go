7a1f016f826ecaff6ec1c5f142915f13a6ef399c
bitqiu <i@bitqiu.cc>
2024-01-16 12:14:23 +0800

    update

--- cmd/ingress.go
+++ cmd/ingress.go
@@ -38,14 +38,4 @@ var ingressCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(ingressCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// ingressCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// ingressCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
